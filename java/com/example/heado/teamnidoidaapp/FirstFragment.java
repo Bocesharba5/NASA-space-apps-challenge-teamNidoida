838c732e0cf1a5eb462c160f8b052168846b4689
Bocesharba <ivana.bocevska@gmail.com>
2018-10-21 14:20:43 +0200

    Initial commit

---
+++ java/com/example/heado/teamnidoidaapp/FirstFragment.java
@@ -0,0 +1,69 @@
+package com.example.heado.teamnidoidaapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.Console;
+
+
+public class FirstFragment extends Fragment{
+    // Store instance variables
+    private String title;
+    private int page;
+
+    // newInstance constructor for creating fragment with arguments
+    public static FirstFragment newInstance(int page, String title) {
+        FirstFragment fragmentFirst = new FirstFragment();
+        Bundle args = new Bundle();
+        args.putInt("someInt", page);
+        args.putString("someTitle", title);
+        fragmentFirst.setArguments(args);
+        return fragmentFirst;
+
+
+    }
+
+    // Store instance variables based on arguments passed
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        page = getArguments().getInt("someInt", 0);
+        title = getArguments().getString("someTitle");
+
+
+    }
+
+    // Inflate the view for the fragment based on layout XML
+    @Override
+    public View onCreateView (LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+        TextView tvLabel = (TextView) view.findViewById(R.id.tvLabel);
+        tvLabel.setText(page + " -- " + title);
+
+        Button button = (Button) view.findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                getActivity().startActivity(new Intent(
+                        getContext(), webViewActivity.class
+                ));
+            }
+        });
+
+        return view;
+
+    }
+
+
+}
---
+++ java/com/example/heado/teamnidoidaapp/webViewActivity.java
@@ -0,0 +1,22 @@
+package com.example.heado.teamnidoidaapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+public class webViewActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.youtube.com/watch?v=TUXui5ItBkM");
+    }
+}
